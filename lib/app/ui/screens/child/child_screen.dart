a7de33771f0515bfbb8c7bac6f22342a9d9c1e85
keldy7 <audmariekouassy@gmail.com>
2023-07-22 05:03:29 +0000

    Essai de refonte de l'interface d'informations-Enfant

--- lib/app/ui/screens/child/child_screen.dart
+++ lib/app/ui/screens/child/child_screen.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 import 'package:scolar_pay/app/data/fake_data.dart';
 import 'package:scolar_pay/app/ui/styles/colors.dart';
 import 'package:scolar_pay/app/utils/constant.dart';
-import 'package:scolar_pay/app/utils/image_keys.dart';
 
 import '../../../utils/label_keys.dart';
 import '../../../utils/routes.dart';
@@ -24,7 +23,8 @@ class ChildScreen extends StatefulWidget {
 class _ChildScreenState extends State<ChildScreen> {
   @override
   Widget build(BuildContext context) {
-    double horSpace = Constant.getDefaultHorSpaceFigma(context) - 5;
+    double height = MediaQuery.of(context).size.height;
+    // double horSpace = Constant.getDefaultHorSpaceFigma(context) - 5;
     final List<ModelCategory> categoryList = FakeData.getAllCategoryList();
 
     return getScreenDetailDefaultView(context, Labels.detailsEnfantKey, () {
@@ -43,77 +43,223 @@ class _ChildScreenState extends State<ChildScreen> {
                 shrinkWrap: true,
                 padding: EdgeInsets.zero,
                 children: [
-                  10.h.verticalSpace,
                   Container(
-                    width: double.infinity,
-                    height: 151.h,
-                    padding: EdgeInsets.all(10.h),
-                    margin: EdgeInsets.symmetric(
-                        horizontal: horSpace, vertical: 10.h),
-                    decoration: getButtonDecoration(getCardColor(context),
-                        withCorners: true,
-                        corner: 20.h,
-                        withBorder: true,
-                        borderColor: secondaryColor,
-                        shadow: [
-                          const BoxShadow(
-                              color:
-                                  Color.fromRGBO(0, 0, 0, 0.07999999821186066),
-                              offset: Offset(-4, 5),
-                              blurRadius: 16)
-                        ]),
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 80.h,
-                          height: double.infinity,
-                          child: Stack(
-                            children: [
-                              getCircularImage(context, double.infinity,
-                                  double.infinity, 20.h, Images.boyPng,
-                                  boxFit: BoxFit.cover),
-                            ],
-                          ),
-                        ),
-                        18.w.horizontalSpace,
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              getCustomFont("Nom complet - Enfant ", 18,
-                                  getFontColor(context), 2,
-                                  fontWeight: FontWeight.w700),
-                              const SizedBox(height: 1.0),
-                              getCustomFont(
-                                  "${Constant.addColonToLabel(Labels.ecoleKey)} Groupe Scolaire Les orchidées de Kouté",
-                                  15,
-                                  getFontGreyColor(context),
-                                  3,
-                                  fontWeight: FontWeight.w600),
+                      height: height * 0.43,
+                      child: LayoutBuilder(builder: (context, constraints) {
+                        double innerHeight = constraints.maxHeight;
+                        double innerWidth = constraints.maxWidth;
+                        return Stack(fit: StackFit.expand, children: [
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              height: innerHeight * 0.6,
+                              width: innerWidth,
+                              decoration: getButtonDecoration(
+                                  getCardColor(context),
+                                  withCorners: true,
+                                  corner: 20.h,
+                                  withBorder: true,
+                                  borderColor: secondaryColor,
+                                  shadow: [
+                                    const BoxShadow(
+                                        color: Color.fromRGBO(
+                                            0, 0, 0, 0.07999999821186066),
+                                        offset: Offset(-4, 5),
+                                        blurRadius: 16)
+                                  ]),
+                              // decoration: BoxDecoration(
+                              //   borderRadius: BorderRadius.circular(30),
+                              //   color: primaryColor,
+                              // ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  Text(
+                                    'KONAN',
+                                    style: TextStyle(
+                                        color: blackColor,
+                                        fontFamily: 'Nunito',
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  getCustomFont("Affoué Edwige Roxane", 18,
+                                      getFontGreyColor(context), 1,
+                                      fontWeight: FontWeight.w600),
+                                  const SizedBox(
+                                    height: 2,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Column(
+                                        children: [
+                                          getCustomFont("Classe", 18,
+                                              getFontGreyColor(context), 1,
+                                              fontWeight: FontWeight.w500),
+                                          const SizedBox(
+                                            height: 2,
+                                          ),
+                                          const Text(
+                                            'CE1',
+                                            style: TextStyle(
+                                                color: Color.fromRGBO(
+                                                    39, 105, 171, 1),
+                                                fontFamily: 'Nunito',
+                                                fontSize: 19,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ],
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 25,
+                                          vertical: 8,
+                                        ),
+                                        child: Container(
+                                          height: 50,
+                                          width: 3,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(100),
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                      ),
+                                      Column(
+                                        children: [
+                                          getCustomFont("Age", 18,
+                                              getFontGreyColor(context), 1,
+                                              fontWeight: FontWeight.w500),
+                                          const SizedBox(
+                                            height: 2,
+                                          ),
+                                          const Text(
+                                            '9 ans',
+                                            style: TextStyle(
+                                                color: Color.fromRGBO(
+                                                    39, 105, 171, 1),
+                                                fontFamily: 'Nunito',
+                                                fontSize: 19,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
                                   getCustomFont(
-                                  "${Constant.addColonToLabel(Labels.classeKey)} CE2",
-                                  15,
-                                  getFontGreyColor(context),
-                                  1,
-                                  fontWeight: FontWeight.w600),
-                              getCustomFont(
-                                  "${Constant.addColonToLabel(Labels.academicYearKey)} 2023-2024",
-                                  15,
-                                  getFontGreyColor(context),
-                                  1,
-                                  fontWeight: FontWeight.w600),
-                            ],
+                                          "${Constant.addColonToLabel(Labels.ecoleKey)} Groupe Scolaire Les orchidées de Kouté",
+                                          15,
+                                          getFontGreyColor(context),
+                                          3,
+                                          fontWeight: FontWeight.w600)
+                                      .paddingSymmetric(horizontal: 15),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  getCustomFont(
+                                      "${Constant.addColonToLabel(Labels.academicYearKey)} 2023-2024",
+                                      15,
+                                      getFontGreyColor(context),
+                                      1,
+                                      fontWeight: FontWeight.w600),
+                                ],
+                              ),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
+                          Positioned(
+                              top: 10,
+                              left: 0,
+                              right: 0,
+                              child: Center(
+                                child: Container(
+                                  child: Image.asset(
+                                    'assets/images/png/girl.png',
+                                    width: innerWidth * 0.45,
+                                    height: innerHeight * 0.5,
+                                    fit: BoxFit.fitWidth,
+                                  ),
+                                ),
+                              )),
+                        ]);
+                      })),
+
+                  // Container(
+                  //   width: double.infinity,
+                  //   height: 151.h,
+                  //   padding: EdgeInsets.all(10.h),
+                  //   margin: EdgeInsets.symmetric(
+                  //       horizontal: horSpace, vertical: 10.h),
+                  //   decoration: getButtonDecoration(getCardColor(context),
+                  //       withCorners: true,
+                  //       corner: 20.h,
+                  //       withBorder: true,
+                  //       borderColor: secondaryColor,
+                  //       shadow: [
+                  //         const BoxShadow(
+                  //             color:
+                  //                 Color.fromRGBO(0, 0, 0, 0.07999999821186066),
+                  //             offset: Offset(-4, 5),
+                  //             blurRadius: 16)
+                  //       ]),
+                  //   child: Row(
+                  //     children: [
+                  //       Container(
+                  //         width: 80.h,
+                  //         height: double.infinity,
+                  //         child: Stack(
+                  //           children: [
+                  //             getCircularImage(context, double.infinity,
+                  //                 double.infinity, 20.h, Images.boyPng,
+                  //                 boxFit: BoxFit.cover),
+                  //           ],
+                  //         ),
+                  //       ),
+                  //       18.w.horizontalSpace,
+                  //       // Expanded(
+                  //       //   child: Column(
+                  //       //     crossAxisAlignment: CrossAxisAlignment.start,
+                  //       //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //       //     children: [
+                  //       //       getCustomFont("Nom complet - Enfant ", 18,
+                  //       //           getFontColor(context), 2,
+                  //       //           fontWeight: FontWeight.w700),
+                  //       //       const SizedBox(height: 1.0),
+                  //       //       getCustomFont(
+                  //       //           "${Constant.addColonToLabel(Labels.ecoleKey)} Groupe Scolaire Les orchidées de Kouté",
+                  //       //           15,
+                  //       //           getFontGreyColor(context),
+                  //       //           3,
+                  //       //           fontWeight: FontWeight.w600),
+                  //       //           getCustomFont(
+                  //       //           "${Constant.addColonToLabel(Labels.classeKey)} CE2",
+                  //       //           15,
+                  //       //           getFontGreyColor(context),
+                  //       //           1,
+                  //       //           fontWeight: FontWeight.w600),
+                  //       //       getCustomFont(
+                  //       //           "${Constant.addColonToLabel(Labels.academicYearKey)} 2023-2024",
+                  //       //           15,
+                  //       //           getFontGreyColor(context),
+                  //       //           1,
+                  //       //           fontWeight: FontWeight.w600),
+                  //       //     ],
+                  //       //   ),
+                  //       // ),
+
+                  //     ],
+                  //   ),
+                  // ),
+
                   20.h.verticalSpace,
                   Container(
                     width: double.infinity,
-                    padding:
-                        EdgeInsets.only(right: 15.w, left: 15.w, bottom: 22.w),
+                    padding: EdgeInsets.only(bottom: 22.w),
                     child: LayoutBuilder(
                       builder: (BuildContext context,
                           BoxConstraints boxConstraints) {
@@ -153,11 +299,9 @@ class _ChildScreenState extends State<ChildScreen> {
             getButtonFigma(context, getAccentColor(context), true,
                 "Modifier les informations", primaryColor, () {
               Constant.goToNextPage(context, Routes.editChildScreenRoute);
-            },
-                EdgeInsets.only(
-                    left: horSpace, right: horSpace, bottom: 30.h, top: 7.h))
+            }, EdgeInsets.only(bottom: 30.h, top: 7.h))
           ],
-        ),
+        ).paddingSymmetric(horizontal: 15),
         centerTitle: true);
   }
 }
--- lib/app/ui/screens/child/edit_child_screen.dart
+++ lib/app/ui/screens/child/edit_child_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:scolar_pay/app/ui/styles/colors.dart';
 import 'package:scolar_pay/app/utils/constant.dart';
 import 'package:scolar_pay/app/utils/widget_utils.dart';
+
+import '../../../utils/label_keys.dart';
 
 class EditChildScreen extends StatefulWidget {
   const EditChildScreen({super.key});
@@ -12,15 +17,191 @@ class EditChildScreen extends StatefulWidget {
 class _EditChildScreenState extends State<EditChildScreen> {
   @override
   Widget build(BuildContext context) {
-    return getScreenDetailDefaultView(context, "Profil : Mon enfant", () {
-      Constant.backToPrev(context);
-    },
-        Column(
+        double width = MediaQuery.of(context).size.width;
+
+    double height = MediaQuery.of(context).size.height;
+    return getScreenDetailDefaultView(
+      context,
+      "Profil : Mon enfant",
+      () {
+        Constant.backToPrev(context);
+      },
+      Column(children: [
+        Expanded(
+            child: ListView(
+          shrinkWrap: true,
           children: [
-            Expanded(
-                child: ListView(
-                    shrinkWrap: true, children: [Text("child edit screen")]))
+            const Text("child edit screen"),
+            Container(
+                height: height * 0.43,
+                child: LayoutBuilder(builder: (context, constraints) {
+                  double innerHeight = constraints.maxHeight;
+                  double innerWidth = constraints.maxWidth;
+                  return Stack(fit: StackFit.expand, children: [
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        height: innerHeight * 0.6,
+                        width: innerWidth,
+                        decoration: getButtonDecoration(getCardColor(context),
+                        withCorners: true,
+                        corner: 20.h,
+                        withBorder: true,
+                        borderColor: secondaryColor,
+                        shadow: [
+                          const BoxShadow(
+                              color:
+                                  Color.fromRGBO(0, 0, 0, 0.07999999821186066),
+                              offset: Offset(-4, 5),
+                              blurRadius: 16)
+                        ]),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              'KONAN',
+                              style: TextStyle(
+                                color: blackColor,
+                                fontFamily: 'Nunito',
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                            Text(
+                              'Affoué Edwige Roxane',
+                              style: TextStyle(
+                                color: black40Color,
+                                fontFamily: 'Nunito',
+                                fontSize: 17,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text(
+                                      'Classe',
+                                      style: TextStyle(
+                                        color: Colors.grey[700],
+                                        fontFamily: 'Nunito',
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    const Text(
+                                      'CE1',
+                                      style: TextStyle(
+                                        color: Color.fromRGBO(39, 105, 171, 1),
+                                        fontFamily: 'Nunito',
+                                        fontSize: 19,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 25,
+                                    vertical: 8,
+                                  ),
+                                  child: Container(
+                                    height: 50,
+                                    width: 3,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(100),
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'Age',
+                                      style: TextStyle(
+                                        color: Colors.grey[700],
+                                        fontFamily: 'Nunito',
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    const Text(
+                                      '9 ans',
+                                      style: TextStyle(
+                                        color: Color.fromRGBO(39, 105, 171, 1),
+                                        fontFamily: 'Nunito',
+                                        fontSize: 19,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                
+                                
+                                
+                              ],
+                            ),
+                            getCustomFont(
+                                  "${Constant.addColonToLabel(Labels.ecoleKey)} Groupe Scolaire Les orchidées de Kouté",
+                                  15,
+                                  getFontGreyColor(context),
+                                  3,
+                                  fontWeight: FontWeight.w600).paddingSymmetric(horizontal: 15),
+                             getCustomFont(
+                                  "${Constant.addColonToLabel(Labels.academicYearKey)} 2023-2024",
+                                  15,
+                                  getFontGreyColor(context),
+                                  1,
+                                  fontWeight: FontWeight.w600),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                                    top: 10,
+                                    left: 0,
+                                    right: 0,
+                                    child: Center(
+                                      child: Container(
+                                        child: Image.asset(
+                                          'assets/images/png/boy.png',
+                                          width: innerWidth * 0.45,
+                                          height: innerHeight * 0.5,
+                                          fit: BoxFit.fitWidth,
+                                        ),
+                                      ),
+                                    )),
+                                    
+                  ]);
+                })),
+                const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: height * 0.5,
+                    width: width,
+                    decoration: getButtonDecoration(getCardColor(context),
+                        withCorners: true,
+                        corner: 20.h,
+                        withBorder: true,
+                        borderColor: secondaryColor,
+                        shadow: [
+                          const BoxShadow(
+                              color:
+                                  Color.fromRGBO(0, 0, 0, 0.07999999821186066),
+                              offset: Offset(-4, 5),
+                              blurRadius: 16)
+                        ]),
+                    child: const Text('test').paddingAll(5))
           ],
-        ));
+        ))
+      ]).paddingSymmetric(horizontal: 15),
+    );
   }
 }
