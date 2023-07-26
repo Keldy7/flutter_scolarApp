8699d35d563a9bedf18be81afc82eac24eadfa6a
keldy7 <audmariekouassy@gmail.com>
2023-07-26 04:43:58 +0000

    Listing statique des examens et compos

--- lib/app/ui/screens/exams/exams_screen.dart
+++ lib/app/ui/screens/exams/exams_screen.dart
@@ -35,84 +35,93 @@ class _ExamenScreenState extends State<ExamenScreen> {
                 20.h.verticalSpace,
                 //janv-feb text
                 buildTitles(context, "Jan - Fev"),
-                Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8)),
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    elevation: 0,
-                    color: primaryColor,
-                    shadowColor: shadowColor,
-                    margin: const EdgeInsets.all(5),
-                    child: InkWell(
-                      onTap: () {
-                        Constant.goToNextPage(
-                            context, Routes.examsDetailScreenRoute);
-                      },
-                      child: SizedBox(
-                          height: 100,
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Container(
-                                width: 10,
-                                color: secondaryColor,
-                              ),
-                              10.w.horizontalSpace,
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    getCustomFont(
-                                        "Titre compo", 16, blackColor, 2,
-                                        fontWeight: FontWeight.bold),
-                                    const Spacer(),
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Icon(
-                                          Icons.calendar_month,
-                                          size: 18.h,
-                                          color: accentColor,
-                                        ),
-                                        8.w.horizontalSpace,
-                                        getCustomFont(
-                                            "Vendredi, 22 Juillet 2023",
-                                            14,
-                                            greyFontColor,
-                                            1),
-                                      ],
-                                    ),
-                                    5.h.verticalSpace,
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Icon(
-                                          Icons.access_time_outlined,
-                                          size: 18.h,
-                                          color: accentColor,
-                                        ),
-                                        8.w.horizontalSpace,
-                                        getCustomFont(
-                                            "09H - 11H", 14, greyFontColor, 1),
-                                      ],
-                                    ),
-                                  ],
-                                ).paddingOnly(top: 15, bottom: 15),
-                              ),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  const Text(
-                                    "Assez-bien",
-                                    maxLines: 2,
+                Column(
+                  children: List.generate(7, (int _) {
+                    return Card(
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: greyColor
+                          ),
+                            borderRadius: BorderRadius.circular(8)),
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        elevation: 0,
+                        color: primaryColor,
+                        shadowColor: shadowColor,
+                        margin: const EdgeInsets.all(5),
+                        child: InkWell(
+                          onTap: () {
+                            Constant.goToNextPage(
+                                context, Routes.examsDetailScreenRoute);
+                          },
+                          child: SizedBox(
+                              height: 100,
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Container(
+                                    width: 10,
+                                    color: secondaryColor,
                                   ),
-                                  5.h.verticalSpace,
+                                  10.w.horizontalSpace,
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        getCustomFont(
+                                            "Titre compo", 16, blackColor, 2,
+                                            fontWeight: FontWeight.bold),
+                                        const Spacer(),
+                                        Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Icon(
+                                              Icons.calendar_month,
+                                              size: 18.h,
+                                              color: accentColor,
+                                            ),
+                                            8.w.horizontalSpace,
+                                            getCustomFont(
+                                                "Vendredi, 22 Juillet 2023",
+                                                14,
+                                                greyFontColor,
+                                                1),
+                                          ],
+                                        ),
+                                        5.h.verticalSpace,
+                                        Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Icon(
+                                              Icons.access_time_outlined,
+                                              size: 18.h,
+                                              color: accentColor,
+                                            ),
+                                            8.w.horizontalSpace,
+                                            getCustomFont("09H - 11H", 14,
+                                                greyFontColor, 1),
+                                          ],
+                                        ),
+                                      ],
+                                    ).paddingOnly(top: 15, bottom: 15),
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      const Text(
+                                        "Assez-bien",
+                                        maxLines: 2,
+                                      ),
+                                      5.h.verticalSpace,
+                                    ],
+                                  ).paddingOnly(right: 10)
                                 ],
-                              ).paddingOnly(right: 10)
-                            ],
-                          )),
-                    ))
+                              )),
+                        )).paddingOnly(bottom: 10);
+                  }),
+                )
               ],
             ))
           ],
