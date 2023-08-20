a581fb7712913be19ce46866f5e23e8982470377
keldy7 <audmariekouassy@gmail.com>
2023-08-20 23:58:13 +0000

    screens: Réalisation de l'emploi du temps

--- lib/app/ui/screens/timetable/timetable_screen.dart
+++ lib/app/ui/screens/timetable/timetable_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scolar_pay/app/utils/constant.dart';
+// import 'package:time_planner/time_planner.dart';
 
 import '../../../utils/widget_utils.dart';
 
@@ -19,6 +20,65 @@ class _TimetableScreenState extends State<TimetableScreen> {
     }, Column(
       children: [
         Text("Emploi du temps"),
+        TimePlanner(
+          startHour: 2,
+          endHour: 24,
+          headers: [
+            TimePlannerTitle(
+              date: "7/20/2021",
+              title: "tuesday",
+            ),
+            TimePlannerTitle(
+              date: "7/21/2021",
+              title: "wednesday",
+            ),
+            TimePlannerTitle(
+              date: "7/22/2021",
+              title: "thursday",
+            ),
+            TimePlannerTitle(
+              date: "7/23/2021",
+              title: "friday",
+            ),
+            TimePlannerTitle(
+              date: "7/24/2021",
+              title: "saturday",
+            ),
+            TimePlannerTitle(
+              date: "7/25/2021",
+              title: "sunday",
+            ),
+            TimePlannerTitle(
+              date: "7/26/2021",
+              title: "monday",
+            ),
+            TimePlannerTitle(
+              date: "7/27/2021",
+              title: "tuesday",
+            ),
+            TimePlannerTitle(
+              date: "7/28/2021",
+              title: "wednesday",
+            ),
+            TimePlannerTitle(
+              date: "7/29/2021",
+              title: "thursday",
+            ),
+            TimePlannerTitle(
+              date: "7/30/2021",
+              title: "friday",
+            ),
+            TimePlannerTitle(
+              date: "7/31/2021",
+              title: "Saturday",
+            ),
+          ],
+          tasks: tasks,
+          style: TimePlannerStyle(
+              showScrollBar: true
+          ),
+        ),
+      
       ],
     ));
   }
